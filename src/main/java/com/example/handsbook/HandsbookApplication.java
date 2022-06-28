a9f38eb05de6c932eb202a7be6b751bddddccbd6
pskudry <polynachik@gmail.com>
2022-06-28 23:05:49 +0600

    проба

---
+++ src/main/java/com/example/handsbook/HandsbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.handsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsbookApplication.class, args);
+    }
+
+}
